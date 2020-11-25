7ba45bdfbb62805c01aa544c3e7669f011431f58
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-11-26 03:44:29 +0530

    Changes

---
+++ src/components/login.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import "../styles/App.css";
+import Welcome from "./welcome";
+import ErrorMessage from "./errorMessage";
+
+export default class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      phNo: "",
+      password: "",
+      errorMessage: "",
+      userName: ""
+    };
+  }
+
+  handleNameChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  handlePhoneNoChange = (event) => {
+    this.setState({ phNo: event.target.value });
+  };
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleChangeValue = (event) => {};
+
+  handleSubmit = () => {
+    const alphanumeric = /^[0-9a-zA-Z ]+$/;
+    const numbers = /^\d+$/;
+    if (
+      this.state.name === "" ||
+      this.state.email === "" ||
+      this.state.phNo === "" ||
+      this.state.password === ""
+    ) {
+      this.setState({ errorMessage: "All fields are mandatory" });
+      return;
+    }
+    if (!this.state.name.match(alphanumeric)) {
+      this.setState({ errorMessage: "Name is not alphanumeric" });
+      return;
+    }
+    if (this.state.email.indexOf("@") < 1) {
+      this.setState({ errorMessage: "Email must contain @" });
+      return;
+    }
+    if (!numbers.test(this.state.phNo)) {
+      this.setState({ errorMessage: "Phone Number must contain only numbers" });
+      return;
+    }
+    if (this.state.password.length < 6) {
+      this.setState({
+        errorMessage: "Password must contain atleast 6 letters"
+      });
+      return;
+    }
+    const user = this.state.email.substring(0, this.state.email.indexOf("@"));
+    this.setState({
+      userName: user,
+      errorMessage: "",
+      name: "",
+      email: "",
+      phNo: "",
+      password: ""
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <input
+          data-testid="name"
+          type="text"
+          placeholder="Name"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
+        <input
+          data-testid="email"
+          type="text"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
+        <div data-testid="gender" onChange={this.handleChangeValue}>
+          <input type="radio" value="male" name="gender" checked="checked" />
+          <label for="male">Male</label>
+          <input type="radio" value="female" name="gender" />
+          <label for="female">Female</label>
+          <input type="radio" value="other" name="gender" />
+          <label for="other">Other</label>
+        </div>
+        <input
+          data-testid="phoneNumber"
+          type="text"
+          placeholder="Phone Number"
+          value={this.state.phNo}
+          onChange={this.handlePhoneNoChange}
+        />
+        <input
+          data-testid="password"
+          type="password"
+          placeholder="Password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
+        <button data-testid="submit" onClick={this.handleSubmit}>
+          Submit
+        </button>
+        {this.state.errorMessage && (
+          <ErrorMessage error={this.state.errorMessage} />
+        )}
+        {this.state.userName && <Welcome userName={this.state.userName} />}
+      </>
+    );
+  }
+}
---
+++ src/components/welcome.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Welcome(props) {
+  return <div>Hello {props.userName}</div>;
+}
